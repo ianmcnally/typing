06c8561d0c8f9b09cdff47ef7d510b1f262914f4
Ian McNally <imcnally@stridenyc.com>
2016-03-01 11:55:22 -0500

    add words and word test cases :D

--- src/__tests__/app_test.js
+++ src/__tests__/app_test.js
@@ -1,4 +1,4 @@
-import { App, Words } from 'src/app'
+import { App, Words, Word } from 'src/app'
 import renderShallow from 'src/test-helpers/lib/render-shallow'
 import React from 'react'
 import { expect } from 'chai'
@@ -17,6 +17,42 @@ describe('App', () => {
       expect(component).to.include(
         <Words words={['a', 'bee']} />
       )
+    })
+
+  })
+
+  describe('<Words>', () => {
+    const words = ['hey', 'ian']
+    let component
+
+    before(() => {
+      const { output } = renderShallow(<Words words={words} />)
+      component = output
+    })
+
+    it('renders a <Word> for every props.words', () => {
+      words.forEach((word, index) => {
+        expect(component).to.include(<Word value={word} index={index} />)
+      })
+    })
+
+    it('has a container of type <article>', () => {
+      expect(component.type).to.equal('article')
+    })
+
+  })
+
+  describe('<Word>', () => {
+    const value = 'Ey there Charlie boy'
+    let component
+
+    before(() => {
+      const { output } = renderShallow(<Word value={value} />)
+      component = output
+    })
+
+    it('renders props.value as text wrapped in a span', () => {
+      expect(component).to.eql(<span>{value}</span>)
     })
 
   })
