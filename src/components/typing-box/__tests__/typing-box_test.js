2deb512792433e7260d0952774b13825055c18a9
Ian McNally <imcnally@stridenyc.com>
2016-03-02 17:50:17 -0500

    simulate actual events on typing box input test

--- src/components/typing-box/__tests__/typing-box_test.js
+++ src/components/typing-box/__tests__/typing-box_test.js
@@ -1,20 +1,53 @@
 import React from 'react'
-import { expect } from 'chai'
 import TypingBox from '../typing-box'
+import { findDOMNode } from 'react-dom'
+import { expect } from 'chai'
 import { renderShallow } from 'src/test-helpers/lib'
+import { renderIntoDocument, Simulate } from 'react-addons-test-utils'
+import defer from 'lodash.defer'
+
+const noRefCheck = () => {}
 
 describe('<TypingBox>', () => {
-  let component
 
-  before(() => {
-    const { output } = renderShallow(<TypingBox />)
-    component = output
+  context('when it renders', () => {
+    let component
+
+    before(() => {
+      const { output } = renderShallow(<TypingBox />)
+      component = output
+    })
+
+    it('renders an input element', () => {
+      expect(component).to.eql(
+        <input type='text' autoFocus onKeyDown={noRefCheck} />
+      )
+    })
+
   })
 
-  it('renders an input element', () => {
-    expect(component).to.eql(
-      <input type='text' autoFocus/>
-    )
+  context('when the spacebar is pressed', () => {
+    let input
+
+    before(done => {
+      const component = renderIntoDocument(<TypingBox />)
+      input = findDOMNode(component)
+
+      input.value = 'zzz'
+      Simulate.change(input)
+
+      Simulate.keyDown(input, { keyCode : 32 })
+
+      defer(done)
+    })
+
+    it('clears the input text', () => {
+      expect(input.value).to.be.empty
+    })
+
+    xit('dispatches a word advancement action', () => {
+    })
+
   })
 
 })
--- src/components/typing-box/typing-box.jsx
+++ src/components/typing-box/typing-box.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
+import defer from 'lodash.defer'
 
-const TypingBox = () => (
-  <input type='text' autoFocus />
-)
+const SPACEBAR_KEY_CODE = 32
 
-export default TypingBox
+export default class TypingBox extends Component {
+  constructor (props) {
+    super(props)
+
+    this.onKeyDown = this.onKeyDown.bind(this)
+  }
+
+  onKeyDown (event) {
+    const { keyCode, target } = event
+
+    if (keyCode !== SPACEBAR_KEY_CODE)
+      return
+
+    defer(() => target.value = '')
+  }
+
+  render () {
+    return (
+      <input type='text' autoFocus onKeyDown={this.onKeyDown}/>
+    )
+  }
+
+}
