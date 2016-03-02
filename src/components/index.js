8f37671f5c9bd441474776d721f0b8d5c8254710
Ian McNally <imcnally@stridenyc.com>
2016-03-02 18:01:29 -0500

    move <Words> into its own directory

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Word } from './word/word'
-export { default as Words } from './words'
+export { default as Words } from './words/words'
 export { default as TypingBox } from './typing-box/typing-box'
