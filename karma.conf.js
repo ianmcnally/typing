5a555de08c1986917ab960cb5e0fe5814aa379c8
Ian McNally <imcnally@stridenyc.com>
2016-03-04 16:07:24 -0500

    add more files to lint

--- karma.conf.js
+++ karma.conf.js
@@ -5,35 +5,35 @@ var webpackConfig = require('./webpack.config').buildConfig
 
 module.exports = function (config) {
   config.set({
-    basePath : '',
-    frameworks : ['mocha'],
-    files : [
+    basePath: '',
+    frameworks: ['mocha'],
+    files: [
       'node_modules/es5-shim/es5-shim.min.js',
       'node_modules/react/dist/react.min.js',
       {
-        pattern : 'src/test-helpers/test-index.js',
-        watched : false,
-        included : true,
-        served : true
+        pattern: 'src/test-helpers/test-index.js',
+        watched: false,
+        included: true,
+        served: true
       }
     ],
-    webpack : webpackConfig,
-    webpackMiddleware : {
-      stats : {
-        colors : true
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      stats: {
+        colors: true
       },
-      noInfo : true
+      noInfo: true
     },
-    preprocessors : {
-      'src/test-helpers/test-index.js' : ['webpack']
+    preprocessors: {
+      'src/test-helpers/test-index.js': ['webpack']
     },
-    reporters : ['mocha', 'notify'],
-    port : 9876,
-    colors : true,
-    logLevel : config.LOG_INFO,
-    autoWatch : true,
-    browsers : ['PhantomJS2'],
-    singleRun : true
+    reporters: ['mocha', 'notify'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS2'],
+    singleRun: true
 
   })
 }
