c9229a47de8cd11b53b20953f5e3920dbf73456e
Ian McNally <imcnally@stridenyc.com>
2016-03-07 16:15:27 -0500

    use redux-logger in dev

--- src/store.js
+++ src/store.js
@@ -1,13 +1,22 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import * as reducers from 'src/reducers'
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+
+const logger = createLogger({
+  predicate: () => process.env.NODE_ENV === 'development'
+})
 
 const initialState = {
   words: ['a', 'b', 'c']
 }
 
 export const createNewStore = (state = initialState) => (
-  createStore(combineReducers(reducers), state, applyMiddleware(thunk))
+  createStore(
+    combineReducers(reducers),
+    state,
+    applyMiddleware(thunk, logger)
+  )
 )
 
 export default createNewStore()
